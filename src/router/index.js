72e46065fd72f3d0696b1b6f7df8b35cced27a82
Desmond <ywlee9999@hotmail.com>
2022-08-22 02:57:12 +0800

    chore: update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
 
 const routes = [
     {
         path: "/",
         name: "Home",
-        component: () => import("@/views/Home.vue"),
+        component: Home,
     },
     {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import("@/views/NotFound.vue"),
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: () =>
+            import(/* webpackChunkName: "notFound" */ "@/views/NotFound.vue"),
     },
 ];
 
